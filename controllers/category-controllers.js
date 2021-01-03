177a6a2acafe51a1b1f7b31ea069e4fc7c5bce74
Steven Mercer <stevenmercer9913@gmail.com>
2021-01-02 18:05:40 -0800

    category post and index routes are complete

--- controllers/category-controllers.js
+++ controllers/category-controllers.js
@@ -2,14 +2,29 @@ const express = require('express');
 const Category = require('../models/category.js');
 const categories = express.Router();
 
+// NEW
 categories.get('/new', (req, res) => {
   res.render(
     'categories/new.ejs'
   )
 })
 
+//CREATE
+categories.post('/', (req,res) => {
+  Category.create(req.body,  (err, createdCategory) => {
+    res.redirect('/category')
+  })
+})
 
-
+//INDEX
+categories.get('/', (req,res) => {
+  Category.find({}, (err, allCategories) => {
+    res.render(
+      'categories/index.ejs', {
+        categories: allCategories
+      })
+    })
+})
 
 
 module.exports = categories;
